0b8249d832adc3181b4a512c207c8630f49cf460
Samiel <243807@usn.no>
2026-05-05 02:44:48 +0200

    Initial commit: opprettet MVC prosjekt

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BibliotekSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
